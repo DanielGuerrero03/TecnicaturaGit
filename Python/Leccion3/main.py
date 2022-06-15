f1e3ae3045a665272f9c69e89c5f9777a94745b8
Daniel Guerrero <dibu3791@gmail.com>
2022-06-15 14:29:14 -0300

    Añadi los archivos de leccion 3 a la carpeta python

---
+++ Python/Leccion3/main.py
@@ -0,0 +1,44 @@
+# Ciclo while (Mientras o Durante)
+"""contador = 0
+while contador < 78:
+    print('Ejecutamos nuestro ciclo while', contador)
+    contador += 1
+else:
+    print('Fin del ciclo while')"""
+# Imprimir números del 0 al 5 con el ciclo while
+"""maximo = 5
+contador = 0
+while contador <= maximo:
+    print(contador)
+    contador += 1"""
+# ejercicio contador decendente
+"""minimo = 1
+contador = 5
+while contador >= minimo:
+    print(contador)
+    contador -= 1"""
+
+# Ciclo For
+"""cadena = 'Hello'
+for letra in cadena:
+    print(letra)
+else:
+    print('Fin ciclo for')"""
+
+#Palabra reservada break
+"""for letra in 'Alemania':
+    if letra == "a":
+        print(f'Letra encontrada: {letra}')
+        break
+else:
+    print('Fin del ciclo for')"""
+
+# Palabra reservada continue
+for i in range(6):
+    if i % 2 == 0:
+        print(f"Valor: {i}")
+
+for i in range(6):
+    if i % 2 != 0:
+        continue
+    print(f"Valor: {i}")
