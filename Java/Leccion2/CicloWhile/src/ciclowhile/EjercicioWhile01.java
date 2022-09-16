e807d02196c61300913408bf4a39d97276a1daf2
Daniel Guerrero <dibu3791@gmail.com>
2022-09-16 17:28:27 -0300

    Ingresamos las clases de Java con ciclos While, do While y for

--- Java/Leccion2/CicloWhile/src/ciclowhile/EjercicioWhile01.java
+++ Java/Leccion2/CicloWhile/src/ciclowhile/EjercicioWhile01.java
@@ -9,7 +9,18 @@ public class EjercicioWhile01 {
             System.out.println("conteo = " + conteo);
             conteo++; //Vamos aumentando en uno la variable
     }
+        
+        var contador = 0;
+        do{
+            System.out.println("contador = " + contador);
+            contador ++;
+        }
+        while(contador <= 7);
+        
+        for(var contando = 0 ; contando < 7 ; contando++){
+        
+            System.out.println("contando = " + contando);
+        }
+        
     }
 }
-
-//Captura de DANIEL GUERRERO , grupo : Bestias binarias
---
+++ Python/Leccion4/Ejercicio06.py
@@ -0,0 +1,4 @@
+#Ejercicio 6: Tabla de multiplicar
+# hacer un programa que pida un numero por teclado y guarde en una lista
+# su tabla de multiplicar hasta el 10. Por ejemplo:
+# si digita el 5 la lista tendrá :5,10,15,20,25,30,35,40,45,50.
