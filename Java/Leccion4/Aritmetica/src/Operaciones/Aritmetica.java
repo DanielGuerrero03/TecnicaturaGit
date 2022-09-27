59b4af209c52b7bff157bc82caaca089439d9dc2
Daniel Guerrero <dibu3791@gmail.com>
2022-09-26 21:01:04 -0300

    Paso de Argumentos en Java y Operaciones
    clase en Java sobre Argumentos y Operador this

---
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,27 @@
+
+package Operaciones;
+
+
+public class Aritmetica {
+    //Atributos de la clase
+    int a;
+    int b;
+    
+    // Metodo
+    public void sumarNumeros(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    
+    public int sumarConRetorno(){
+        int resultado = a + b;
+        return resultado;
+    }
+    
+    public int sumarConArgumentos(int arg1, int arg2){
+        a = arg1;
+        b = arg2;
+        //return a + b;
+        return sumarConRetorno();
+    }
+}
