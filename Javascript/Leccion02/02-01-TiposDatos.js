0a77b2333b2d00e97f2bad2b1af64cb7b3b094a2
Daniel Guerrero <dibu3791@gmail.com>
2022-10-25 21:31:33 -0300

    clase7 js
    acrualizo ejercicios y lecciones de la 1 a la 4

--- Javascript/Leccion02/02-01-TiposDatos.js
+++ Javascript/Leccion02/02-01-TiposDatos.js
@@ -51,3 +51,12 @@ console.log(typeof x);
 // null: significa ausencia de valor
 var y = null; //null no es un tipo de dato, pero su origen es de tipo object
 console.log(typeof y);
+
+//Tipo de dato array y Empty String
+var autos = ['Citroen','Audi','BMW','Ford'];
+console.log(autos);
+console.log(typeof autos); //Preguntamos que tipo de dato es:
+
+var z = '';
+console.log(z); //Esto se refiere a que es una cadena vacia:
+console.log(typeof z);
